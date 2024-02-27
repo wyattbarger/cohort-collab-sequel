92434dab1bab8c8a4dd66c396bbea13775f39f37
Wyatt Barger <wyattbarger@icloud.com>
2024-02-27 13:38:57 -0600

    Add a custom input scheme in the Player class constructor to allow the client to control the sprite with normal WASD computer control scheme. | Added update method to the Player class to handle movement logic for the control scheme within the Player class.

--- shooter-script/public/components/Player.js
+++ shooter-script/public/components/Player.js
@@ -4,5 +4,30 @@ export default class Player extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, "playerSpriteImage");
     scene.add.existing(this);
+
+    // Set the custom controls for the Player class
+    this.cursors = scene.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
   }
+
+  // Add an update method for player game events
+  update() {
+    if (this.cursors.is.up) {
+        this.y += 1;
+    }
+    else if (this.cursors.is.down) {
+        this.y -= 1;
+    }
+    else if (this.cursors.is.left) {
+        this.x -= 1;
+    }
+    else if (this.cursors.is.right) {
+        this.x += 1;
+    }
+  }
+
 }
