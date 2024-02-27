4238993d92446b6e66264838356c39750354b1ba
Wyatt Barger <wyattbarger@icloud.com>
2024-02-27 10:17:21 -0600

    Add debug object to game configuration in main.js. | Set showBounds, showColliders, and showVelocity to true.

--- shooter-script/main.js
+++ shooter-script/main.js
@@ -1,12 +1,17 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
-import Title from '/public/scenes/Title';
-import LevelOne from '/public/scenes/LevelOne';
-import LevelTwo from '/public/scenes/LevelTwo';
+import Title from "/public/scenes/Title";
+import LevelOne from "/public/scenes/LevelOne";
+import LevelTwo from "/public/scenes/LevelTwo";
 
 export default new Phaser.Game({
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: [Title, LevelOne, LevelTwo]
-    });
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [Title, LevelOne, LevelTwo],
+  debug: {
+    showBounds: true,
+    showColliders: true,
+    showVelocity: true,
+  },
+});
