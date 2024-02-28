a040a3cab54454e4fe1011a57dc59ca34df7e004
ahmell <andrewmell10@gmail.com>
2024-02-27 20:01:23 -0600

    changed start screen to begin game on click rather than spacebar

--- shooter-script/public/scenes/Title.js
+++ shooter-script/public/scenes/Title.js
@@ -7,10 +7,16 @@ export default class Title extends Phaser.Scene {
 
   create() {
     this.add.text(350, 200, 'Game', { fill: '#0f0', fontSize: 48, shadow: '2px 2px #000' });
-    this.add.text(300, 400, 'Press Space to Start', { fill: '#0f0' });
-
-    this.input.keyboard.once('keydown-SPACE', () => {
+    const startButton = this.add.text(300, 400, 'Click to Start!', { fill: '#0f0' });
+    startButton.setInteractive();
+    startButton.on('pointerdown', () => {
       this.scene.start('LevelOne');
     });
+
+
+    //remove when start button has functionality
+    // this.input.keyboard.once('keydown-SPACE', () => {
+    //   this.scene.start('LevelOne');
+    // });
   }
 }
