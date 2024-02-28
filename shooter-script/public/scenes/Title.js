ce4e5abe656f437ef27ddb385d32b81770fffe70
ahmell <andrewmell10@gmail.com>
2024-02-27 20:10:09 -0600

    button changes on mouse over/out

--- shooter-script/public/scenes/Title.js
+++ shooter-script/public/scenes/Title.js
@@ -6,12 +6,13 @@ export default class Title extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(350, 200, 'Game', { fill: '#0f0', fontSize: 48, shadow: '2px 2px #000' });
-    const startButton = this.add.text(300, 400, 'Click to Start!', { fill: '#0f0' });
-    startButton.setInteractive();
-    startButton.on('pointerdown', () => {
-      this.scene.start('LevelOne');
-    });
+    this.add.text(350, 200, 'Game', { fill: '#0f0', fontSize: 48, shadow: '2px 2px #000' })
+    const startButton = this.add.text(300, 400, 'Click to Start!', { fill: '#0f0' })
+
+    startButton.setInteractive()
+    .on('pointerdown', () => { this.scene.start('LevelOne');})
+    .on('pointerover', () => { startButton.setStyle({ fill: '#ff0'}); })
+    .on('pointerout', () => { startButton.setStyle({ fill: '#0f0'}); });
 
 
     //remove when start button has functionality
