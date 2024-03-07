98c285063bc67f9d500520d0ca52044015ccd3f0
Wyatt Barger <wyattbarger@icloud.com>
2024-03-06 20:32:15 -0600

    Enemy is now following the player, similar to line 56 of LevelOne.js but instead is stored within the update method of the extended enemy class which is then called in the update method of LevelTwo.js

--- shooter-script/public/components/enemies/leveltwo/Lvl2MeleeEnemy.js
+++ shooter-script/public/components/enemies/leveltwo/Lvl2MeleeEnemy.js
@@ -1,12 +1,15 @@
+import Phaser from "phaser";
 import Enemy from "../../Enemy";
 
 export default class Lvl2MeleeEnemy extends Enemy {
   constructor(scene, x, y) {
     super(scene, x, y, "meleeEnemySprite");
+    this.hitpoints = 2;
+
     this.scene.anims.create({
       key: "movementAnimation",
       frames: [
-        { key: "firstMoveSprite" },
+        { key: "meleeEnemySprite" },
         { key: "secondMoveSprite" },
         { key: "thirdMoveSprite" },
         { key: "fourthMoveSprite" },
@@ -14,9 +17,14 @@ export default class Lvl2MeleeEnemy extends Enemy {
         { key: "sixthMoveSprite" },
         { key: "seventhMoveSprite" },
       ],
+      frameRate: 10,
+      repeat: -1,
     });
+    this.anims.play("movementAnimation")
+
+    this.setCollideWorldBounds(true);
   };
   update () {
-
+    this.scene.physics.moveToObject(this, this.scene.player, 80);
   };
 }
